c1ed596752f72e24f9faa25b5190ddef0caf1cc9
Radek <radek.halfaf@gmail.com>
2020-01-20 18:16:46 +0100

    First commit

---
+++ Kalkulator/src/main/java/sample/Controller.java
@@ -0,0 +1,67 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Controller {
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private Button minus;
+
+    @FXML
+    private Button multi;
+
+    @FXML
+    private Button divide;
+
+    @FXML
+    private TextField textfield;
+
+    @FXML
+    private Button one;
+
+    @FXML
+    private Button four;
+
+    @FXML
+    private Button seven;
+
+    @FXML
+    private Button two;
+
+    @FXML
+    private Button five;
+
+    @FXML
+    private Button eigth;
+
+    @FXML
+    private Button three;
+
+    @FXML
+    private Button six;
+
+    @FXML
+    private Button nine;
+
+    @FXML
+    private Button c;
+
+    @FXML
+    void initialize() {
+        System.out.println();
+
+
+
+    }
+
+
+}
---
+++ Kalkulator/src/main/java/sample/Engine.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Engine {
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int minus(int a, int b){
+        return a-b;
+    }
+    public int multi(int a, int b){
+        return a*b;
+    }
+    public int divide(int a, int b){
+        return a/b;
+    }
+
+}
